f31b4900c746fa8316f41805264a74b391cd3e7f
Anna Baranova <baranova.a.i@yandex.ru>
2021-03-19 12:24:10 +0300

    fix: исправить замечания ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,12 @@
-// открытие-закрытие popup
-
 let popup = document.querySelector('.popup');
 let openPopupBtn = document.querySelector('.profile__edit-button');
-let closePopupBtn = document.querySelector('.form__close-btn');
-let popupOverlay = document.querySelector('.popup__overlay')
+let closePopupBtn = document.querySelector('.popup__close-btn');
+let saveFormButton = document.querySelector('.form__save-btn')
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.form__input_type_name');
+let jobInput = document.querySelector('.form__input_type_job');
+let nameProfile = document.querySelector('.profile__info-title');
+let jobProfile = document.querySelector('.profile__info-subtitle');
 
 function openPopup () {
    popup.classList.add ('popup_visible');
@@ -15,25 +18,13 @@ function closePopup () {
     popup.classList.remove('popup_visible');
 }
 
-openPopupBtn.addEventListener('click', function() {openPopup(); });
-closePopupBtn.addEventListener('click', function() {closePopup(); });
-popupOverlay.addEventListener('click', function() {closePopup(); });
-
-// изменение данных профиля
-
-let saveFormButton = document.querySelector('.form__save-btn')
-let formElement = document.querySelector('.form');
-let nameInput = document.querySelector('.form__input-name');
-let jobInput = document.querySelector('.form__input-job');
-let nameProfile = document.querySelector('.profile__info-title');
-let jobProfile = document.querySelector('.profile__info-subtitle');
-
 function changeData (event) {
     event.preventDefault();
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = jobInput.value;
+    closePopup();
 }
 
+openPopupBtn.addEventListener('click', openPopup);
+closePopupBtn.addEventListener('click', closePopup);
 formElement.addEventListener('submit', changeData);
-
-saveFormButton.addEventListener('click', function() {closePopup(); });
